06206fe1e773c63523e372ffaadd1e6b097e6468
Siyovush <savush.dev92@gmail.com>
2026-05-12 16:36:32 +0500

    Add PLC command client

---
+++ main/plc_client.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "esp_err.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+esp_err_t plc_client_init(void);
+
+esp_err_t plc_client_get_status_ext(void);
+
+esp_err_t plc_client_get_node(uint16_t node_index,
+                              uint8_t *rsp,
+                              size_t rsp_cap,
+                              uint16_t *rsp_len);
+
+esp_err_t plc_client_force_output(uint16_t node_index,
+                                  bool value,
+                                  uint32_t hold_ms);
+
+esp_err_t plc_client_release_output(uint16_t node_index);
+
+esp_err_t plc_client_mem_info(uint8_t *rsp,
+                              size_t rsp_cap,
+                              uint16_t *rsp_len);
+
+esp_err_t plc_client_mem_read(uint8_t mem_type,
+                              uint16_t index,
+                              uint16_t count,
+                              uint8_t *rsp,
+                              size_t rsp_cap,
+                              uint16_t *rsp_len);
+
+#ifdef __cplusplus
+}
+#endif
