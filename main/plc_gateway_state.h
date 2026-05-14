e047ff49e3f3222bf2a5b37c8949c8cdedb6d679
Siyovush <savush.dev92@gmail.com>
2026-05-14 14:19:22 +0500

    Add real PLC node runtime cache

--- main/plc_gateway_state.h
+++ main/plc_gateway_state.h
@@ -7,11 +7,57 @@
 extern "C" {
 #endif
 
+#define PLC_GATEWAY_MAX_NODES 64u
+
+typedef struct {
+    bool valid;
+
+    uint16_t index;
+    uint16_t id;
+    uint32_t type;
+    uint32_t flags;
+
+    bool out_bool;
+    int32_t out_int;
+    float out_float;
+
+    bool force_active;
+    bool force_value;
+    uint32_t force_left_ms;
+
+    bool ton_active;
+    uint32_t ton_accum_ms;
+
+    bool toff_holding;
+    uint32_t toff_left_ms;
+
+    bool sr_q;
+    bool trig_prev;
+
+    int32_t acc;
+    bool prev_clk;
+
+    bool pid_inited;
+    float pid_i;
+    float pid_prev_meas;
+
+    bool filter_inited;
+    float filter_prev;
+
+    bool ramp_inited;
+    float ramp_prev;
+
+    bool ao_zero_hold;
+} plc_gateway_node_state_t;
+
 typedef struct {
     bool connected;
+    bool running;
+    bool active_graph_valid;
     bool safe_or_fault;
 
     uint32_t last_exchange_ms;
+    uint32_t mcu_uptime_ms;
 
     uint32_t cycle_ms;
     uint32_t cycle_counter;
@@ -19,20 +65,29 @@ typedef struct {
     uint32_t max_cycle_us;
 
     uint32_t node_count;
+    uint32_t cached_node_count;
 
     uint32_t active_graph_version;
     uint32_t active_graph_size;
     uint32_t active_graph_crc32;
 
+    uint32_t heap_total;
     uint32_t heap_free;
     uint32_t heap_min;
 
     uint32_t rx_ok;
     uint32_t crc_errors;
+    uint32_t size_errors;
+    uint32_t rx_overflows;
+    uint32_t tx_frames;
+    uint32_t tx_errors;
+    uint32_t uart_errors;
     uint32_t timeouts;
 
     uint32_t runtime_fault;
     uint32_t runtime_fault_counter;
+
+    plc_gateway_node_state_t nodes[PLC_GATEWAY_MAX_NODES];
 } plc_gateway_state_t;
 
 void plc_gateway_state_init(void);
@@ -42,6 +97,10 @@ void plc_gateway_state_get(plc_gateway_state_t *out_state);
 void plc_gateway_state_set_connected(bool connected);
 
 void plc_gateway_state_update_from_status_ext(const uint8_t *body, uint16_t len);
+
+bool plc_gateway_state_update_node_from_node_rsp(const uint8_t *body, uint16_t len);
+
+void plc_gateway_state_clear_nodes(void);
 
 #ifdef __cplusplus
 }
